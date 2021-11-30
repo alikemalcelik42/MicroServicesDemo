9bc3877b4444782bdb90711cb7553134da906e77
alikemalcelik42 <alikemalcelik42@gmail.com>
2021-11-30 18:45:09 +0300

    Add project files.

---
+++ Services/Customer/Services/CustomerService.cs
@@ -0,0 +1,59 @@
+﻿using Infrastructure;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CustomerService : ICustomerService
+    {
+        List<Customer> _customerList;
+
+        public CustomerService()
+        {
+            _customerList = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Id = 1,
+                    FirstName = "Ali",
+                    LastName = "Demir",
+                    BornDate = new DateTime(2000, 4, 2)
+                }
+            };
+        }
+
+        public void Add(Customer customer)
+        {
+            _customerList.Add(customer);
+        }
+
+        public void Delete(Customer customer)
+        {
+            var deletedCustomer = _customerList.FirstOrDefault(c => c.Id == customer.Id);
+            _customerList.Remove(deletedCustomer);
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _customerList;
+        }
+
+        public Customer GetById(int id)
+        {
+            return _customerList.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Customer customer)
+        {
+            var updatedCustomer = _customerList.FirstOrDefault(c => c.Id == customer.Id);
+
+            updatedCustomer.FirstName = customer.FirstName;
+            updatedCustomer.LastName = customer.LastName;
+            updatedCustomer.BornDate = customer.BornDate;
+        }
+    }
+}
---
+++ Services/Customer/WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            return Ok(_customerService.GetAll());
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_customerService.GetById(id));
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Customer customer)
+        {
+            _customerService.Add(customer);
+            return Ok();
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Customer customer)
+        {
+            _customerService.Update(customer);
+            return Ok();
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Customer customer)
+        {
+            _customerService.Delete(customer);
+            return Ok();
+        }
+    }
+}
